ac870b424adbc7c0be02034fc9ff9e30985f9633
Tomohiro Otsuka <t.otsuka@gmail.com>
2012-02-22 01:52:05 +0900

    parse_signed_request()関数のapp_secret引数にunicode文字列が渡された場合の対応。

--- src/strippers/facebook/graphapi.py
+++ src/strippers/facebook/graphapi.py
@@ -602,7 +602,7 @@ def parse_signed_request(signed_request, app_secret):
     if data.get('algorithm', '').upper() != 'HMAC-SHA256':
         return False # raise ValueError('signed_request used unknown algorithm')
 
-    expected_sig = hmac.new(app_secret, msg=payload, digestmod=hashlib.sha256).digest()
+    expected_sig = hmac.new(str(app_secret), msg=payload, digestmod=hashlib.sha256).digest()
     if sig != expected_sig:
         return False # raise ValueError('signed_request had signature mismatch')
 
